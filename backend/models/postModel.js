f7b3dcbf65657a11eea310de7b237cf239383640
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-26 15:32:50 +0530

    Add Post model with comments, likes, and sharing support

---
+++ backend/models/postModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    comments: [commentSchema],
+    sharedForm: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.model('Post',postSchema);
