6c19e7feeaba8cdb70ece84d5779e7a1f1333f9e
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-26 14:23:31 +0530

    ✨ Create userModel with schema and Mongoose setup

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: ''
+    },
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true
+});
+
+
+export default mongoose.model('User',userSchema);
