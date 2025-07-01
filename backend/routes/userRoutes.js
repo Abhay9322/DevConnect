f8d41af86e21b426853974ae3db33cd10e342430
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-01 09:45:15 +0530

    Added follow,unfollow,getAllusers routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,21 @@ const router = express.Router();
 
 import { registerUser, loginUser } from '../controllers/userController.js';
 
+import { followUser,unfollowUser, getAllUsers } from '../controllers/followController.js';
+
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 
+// Follow/unfollow routes
+
+router.put('/follow/:id',protect,followUser)   // PUT /api/users/follow/:id
+router.put('/unfollow/:id',protect,unfollowUser);
+
+
+// Optional: Get all users (to display on frontend)
+
+router.get('/',protect,getAllUsers);
 
 export default router;
