8ec712c8c527cdba628600811d183a8174edd36f
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-07-01 09:06:06 +0530

    Added logic of follow user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import { registerUser, loginUser } from '../controllers/userController.js';
 
+
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 
